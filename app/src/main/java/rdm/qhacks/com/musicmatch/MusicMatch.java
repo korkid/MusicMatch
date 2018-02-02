8d139d7c0ed6a79e95c0accaa82437de0e7531a5
Rahul-Chakravarthy <rahul.chakravarthy101@gmail.com>
2018-02-01 23:10:56 -0500

    App foundations

---
+++ app/src/main/java/rdm/qhacks/com/musicmatch/MusicMatch.java
@@ -0,0 +1,13 @@
+package rdm.qhacks.com.musicmatch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MusicMatch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_music_match);
+    }
+}

58dc5b3e274af98f8ba7cd4647554aa876d12969
David Gao <David@Davids-MacBook-Air.local>
2018-02-03 14:15:33 -0500

    Added UI changes

--- app/src/main/java/rdm/qhacks/com/musicmatch/Activities/MusicMatch.java
+++ app/src/main/java/rdm/qhacks/com/musicmatch/Activities/MusicMatch.java
@@ -1,15 +1,61 @@
 package rdm.qhacks.com.musicmatch.Activities;
 
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
 
 import rdm.qhacks.com.musicmatch.R;
+import rdm.qhacks.com.musicmatch.View.GraphicsDisplayer;
 
-public class MusicMatch extends AppCompatActivity {
+public class MusicMatch extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+
+
+
+
         setContentView(R.layout.activity_music_match);
+
+
+        //Initializing shit
+
+        Context context = this;
+        ConstraintLayout cl = findViewById(R.id.MusicMatch);
+
+        GraphicsDisplayer musicMatch = new GraphicsDisplayer(cl, context, this.getWindowManager().getDefaultDisplay());
+
+
+        //Graphics Below
+
+        musicMatch.addText("MusicMatch", "OpenSans-ExtraBoldItalic", "BLACK", 2, 50, 50, 13);
+
+
+
+
+
+
+
+
+
+
+
     }
+
+
+
+
+
+
+
+
 }

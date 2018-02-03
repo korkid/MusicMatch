8d1eec081f4e98bbe8aede3842ba43fe64bc618f
David Gao <David@Davids-MacBook-Air.local>
2018-02-03 01:00:20 -0500

    Added graphics library

---
+++ MaterialLibraryUI/src/main/java/com/example/materiallibraryui/myClass.java
@@ -0,0 +1,4 @@
+package com.example.materiallibraryui;
+
+public class myClass {
+}
--- app/src/main/java/rdm/qhacks/com/musicmatch/MusicMatch.java
+++ app/src/main/java/rdm/qhacks/com/musicmatch/MusicMatch.java
@@ -1,7 +1,7 @@
 package rdm.qhacks.com.musicmatch;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MusicMatch extends AppCompatActivity {
 
